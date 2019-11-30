8b10dd6814d3924fbdbbe9a778ac25c4aea2ba5c
kiril <kirill.cska.slezin@gmail.com>
2019-11-30 14:45:49 +0300

    Я решил задачу №1 из 2 урока

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+
+pygame.init()
+size = width, height = 800, 600
+screen = pygame.display.set_mode(size)
+running = True
+
+x, y, r = -1, -1, 0
+speed = 10
+grow = False
+timer = pygame.time.Clock()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            r = 0
+            grow = True
+    if grow:
+        r += speed * timer.tick() / 1000
+    screen.fill(pygame.Color('black'))
+    pygame.draw.circle(screen, pygame.Color('yellow'), (x, y), int(r))
+    pygame.display.flip()
